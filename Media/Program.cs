057ad04e6b916a129cfad24d76dfea650292aa5e
Vitor Lassen <vitor@lassen.com.br>
2026-07-07 22:10:10 -0300

    S2A1

---
+++ Media/Program.cs
@@ -0,0 +1,76 @@
+﻿Console.WriteLine("Bem vindo ao programa de calculo de media: ");
+
+Console.Write("Digite uma Media para começar: ");
+double nota1 = double.Parse(Console.ReadLine());
+
+Console.Write("Digite a segunda nota: ");
+double nota2 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Digite a ultima nota: ");
+double nota3 = Convert.ToDouble(Console.ReadLine());
+//Existe tambem o casting que seria um parentese forçando o tipo : (double)
+
+double media = (nota1 + nota2 + nota3) / 3;
+
+// Vamos implementar a logica de aprovação do aluno, baseado na media calculada
+
+
+// Se Média >= 7 = Aluno Aprovado
+// Se Média  >= 5  e < 7  Em recuperação
+// Se Media <5  Reprovado
+
+
+// cenario com maior validação usando somente ifs 
+// if (media >= 7)
+// {
+//     Console.WriteLine($"Aluno aprovado! com média de: {media:F2}");
+// }
+
+// if (media >=5)
+// {
+//     if (media < 7)
+//     {
+//         Console.WriteLine($"Aluno em recuperacao! com média de: {media:F2}");
+//     }
+// }
+
+// if (media <5)
+// {
+//     Console.WriteLine($"Aluno Reprovado! com média de: {media:F2}");
+// }
+
+
+//cenario parcial usando. if else 
+if (media >= 7)
+{
+    Console.WriteLine($"Aluno aprovado! com média de: {media:F2}");
+}
+else {
+    if (media >=5)
+    {
+        Console.WriteLine($"Aluno em recuperacao! com média de: {media:F2}");
+    }
+}
+
+if (media<5)
+{
+    Console.WriteLine($"Aluno Reprovado! com média de: {media:F2}");
+}
+
+//cenário mais otimizado usando else if 
+if (media >= 7)
+{
+    Console.WriteLine($"Aluno aprovado! com média de: {media:F2}");
+}
+else {
+    if (media >=5)
+    {
+        Console.WriteLine($"Aluno em recuperacao! com média de: {media:F2}");
+    }
+    else 
+    {
+        Console.WriteLine($"Aluno Reprovado! com média de: {media:F2}");
+    }
+}
+
+
---
+++ exercicios/Program.cs
@@ -0,0 +1,28 @@
+﻿//Converter temperatura Celsius para Fahrenheit.    F=C⋅9/5+32
+Console.Write("Digite a temperatira em celsius para ser convertida: ");
+
+float celsius = float.Parse(Console.ReadLine());
+
+float fahrenheit = celsius * 9/5 +32;
+
+Console.WriteLine($"O valor de {celsius:F2} Celsius em Fahrenheit é de {fahrenheit:F2}");
+
+
+//Par ou Ímpar: Usar o operador de módulo (%).
+
+Console.WriteLine("Digite o valor para saber se ele é par ou impar");
+
+
+//Parse server para converter o texto lido que o usuario escrever para o tipo da variavel.
+int numero = int.Parse(Console.ReadLine());
+
+int ehPar = numero % 2;
+
+if (ehPar == 0)
+{
+    Console.WriteLine($"O numero {numero} é Par");
+}
+else
+{
+    Console.WriteLine($"O numero {numero} é impar");
+}
